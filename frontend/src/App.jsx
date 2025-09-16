014711a4159396356407f7ecdfc8a8d3efb0b0f4
EJDonato <eltonjamesdonato05@gmail.com>
2025-09-17 00:07:47 +0800

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        LUNA - My Personal Assistant
+      </div>
+    </>
+  )
+}
+
+export default App
